643c443a6e9582ab0626051b4effcb12bf98ff11
captaink <wcaptaink@gmail.com>
2022-07-02 21:38:09 +0900

    14 Building the User Interface

--- InstagramSwiftUITutorial/View/UploadPost/UploadPostView.swift
+++ InstagramSwiftUITutorial/View/UploadPost/UploadPostView.swift
@@ -8,8 +8,45 @@
 import SwiftUI
 
 struct UploadPostView: View {
+    @State private var selectedImage: UIImage?
+    @State var postImage: Image?
+    @State var captionText = ""
+    
     var body: some View {
-        Text("Upload Post")
+        VStack {
+            if postImage == nil {
+                Button(action: {}, label: {
+                    Image(systemName: "plus.app")
+                        .resizable()
+                        .renderingMode(.template) // 이 옵션을 사용하면 원래 색깔을 무시하고 내가 원하는 색깔로 이미지에 덧칠할 수 있음
+                        .scaledToFill()
+                        .frame(width: 180, height: 180)
+                        .clipped()
+                        .padding(.top, 56)
+                        .foregroundColor(.black)
+                })
+            } else {
+                HStack(alignment: .top) {
+                    Image("Mackenzie")
+                        .resizable()
+                        .scaledToFill()
+                        .frame(width: 96, height: 96)
+                        .clipped()
+                    
+                    TextField("Enter your caption..", text:
+                                $captionText)
+                }
+                Button(action: {}, label: {
+                    Text("Share")
+                        .font(.system(size: 16, weight: .semibold))
+                        .frame(width: 360, height: 50)
+                        .background(.blue)
+                        .cornerRadius(5)
+                        .foregroundColor(.white)
+                }).padding()
+            }
+            Spacer()
+        }.padding()
     }
 }
 
